6735c7bb4cf41e146a8b1cd6cfc1c6c141fa9394
Irouane1 <itsirw@gmail.com>
2024-01-30 18:22:06 +0100

    c

--- javascript-warm_up/103-object_fct.js
+++ javascript-warm_up/103-object_fct.js
@@ -1,25 +1,16 @@
-// 103-object_fct.js
-
-#!/usr/bin/node
 const myObject = {
-  type: 'object',
-  value: 12
-};
-console.log(myObject);
-
-// Create a named function incr
-const incrFunction = function incr() {
-  this.value += 1;
-};
-
-// Add the named function to myObject
-myObject.incr = incrFunction;
-
-myObject.incr();
-console.log(myObject);
-
-myObject.incr();
-console.log(myObject);
-
-myObject.incr();
-console.log(myObject);
+    type: 'object',
+    value: 12,
+    incr: function() {
+      this.value++;
+    }
+  };
+  
+  console.log(myObject);
+  myObject.incr();
+  console.log(myObject);
+  myObject.incr();
+  console.log(myObject);
+  myObject.incr();
+  console.log(myObject);
+  
