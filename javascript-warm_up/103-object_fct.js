92fb54efefe9331de472542b95a5826e4c29d007
Irouane1 <itsirw@gmail.com>
2024-01-30 18:15:39 +0100

    c

--- javascript-warm_up/103-object_fct.js
+++ javascript-warm_up/103-object_fct.js
@@ -1,13 +1,10 @@
-// 103-object_fct.js
-
-#!/usr/bin/node
 const myObject = {
   type: 'object',
   value: 12
 };
 console.log(myObject);
 
-// Add a new function incr to myObject
+
 myObject.incr = function incr() {
   this.value += 1;
 };
