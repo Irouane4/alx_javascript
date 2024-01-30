49d46c101202d7cfea64188b5a579cd522a48c02
Irouane1 <itsirw@gmail.com>
2024-01-30 17:52:27 +0100

    c

---
+++ javascript-warm_up/102-add_me_maybe.js
@@ -0,0 +1,6 @@
+function addMeMaybe(number, theFunction) {
+  number += 1;
+  theFunction(number);
+}
+
+module.exports = { addMeMaybe };
---
+++ javascript-warm_up/102-main.js
@@ -0,0 +1,5 @@
+const { addMeMaybe } = require('./102-add_me_maybe');
+
+addMeMaybe(4, function (nb) {
+  console.log('New value: ' + nb);
+});
