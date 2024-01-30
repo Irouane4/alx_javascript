545d5b0a4e18cb740931f7c17416db34f678dcc0
Irouane1 <itsirw@gmail.com>
2024-01-30 18:17:40 +0100

    c

--- javascript-warm_up/103-object_fct.js
+++ javascript-warm_up/103-object_fct.js
@@ -5,9 +5,11 @@ const myObject = {
 console.log(myObject);
 
 
-myObject.incr = function incr() {
+myObject.incr = function() {
   this.value += 1;
 };
+
+myObject.incr.name = 'incr';
 
 myObject.incr();
 console.log(myObject);
