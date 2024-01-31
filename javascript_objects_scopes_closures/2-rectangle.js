f782b36dd7e152a87198980d1c03b935e84adabc
Irouane1 <itsirw@gmail.com>
2024-01-31 20:59:35 +0100

    c

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,15 +1,11 @@
 #!/usr/bin/node
 class Rectangle {
-    constructor(w, h) {
-      if (w <= 0 || !Number.isInteger(w) || h <= 0 || !Number.isInteger(h)) {
-        this.width = 0;
-        this.height = 0;
-      } else {
-        this.width = w;
-        this.height = h;
-      }
+  constructor(w, h) {
+    if (w <= 0 || h <= 0 || typeof w !== 'number' || typeof h !== 'number') {
+      // Create an empty object
+    } else {
+      this.width = w;
+      this.height = h;
     }
   }
-  
-  module.exports = Rectangle;
-  
+}
