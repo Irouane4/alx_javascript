42d6e243a06795617d2115c319214d405095b939
Irouane1 <itsirw@gmail.com>
2024-02-06 14:33:29 +0100

    c

---
+++ Javascript_advanced/1-nested_functions.js
@@ -0,0 +1,27 @@
+
+var globalVariable = 'Welcome';
+
+function outer() {
+    alert(globalVariable);
+    var course = 'Holberton';
+
+    function inner() {
+        alert(globalVariable + ' ' + course);
+
+        var exclamation = '!';
+
+        function inception() {
+            alert(globalVariable + ' ' + course + exclamation);
+        }
+
+        inception();
+    }
+
+    inner();
+}
+
+outer();
+
+outer();
+
+outer();
